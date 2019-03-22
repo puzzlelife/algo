18c944ef87753300f60250d010beb39e901d9f4a
wangyaowy.wang <wangyaowy.wang@Qunar.com>
2019-03-22 13:31:10 +0800

    Question17

--- src/main/java/leetcode/Question17.java
+++ src/main/java/leetcode/Question17.java
@@ -8,6 +8,15 @@ import java.util.Map;
 /**
  * @Author: wangyaowy.wang
  * @Date 2019/3/22 11:08
+ *
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ *
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ *
+ * 示例:
+ *
+ * 输入："23"
+ * 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
  */
 public class Question17 {
     public List<String> letterCombinations(String digits) {
---
+++ src/main/java/leetcode/Question22.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: wangyaowy.wang
+ * @Date 2019/3/22 13:30
+ *
+ * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+ *
+ * 例如，给出 n = 3，生成结果为：
+ *
+ * [
+ *   "((()))",
+ *   "(()())",
+ *   "(())()",
+ *   "()(())",
+ *   "()()()"
+ * ]
+ */
+public class Question22 {
+    public List<String> generateParenthesis(int n) {
+        List<String> result=new ArrayList<>();
+
+
+        return result;
+    }
+}
