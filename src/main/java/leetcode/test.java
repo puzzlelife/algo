28dbdc53970b50db5d0faf3212eafb88661c0184
wangyaowy.wang <wangyaowy.wang@Qunar.com>
2019-03-27 16:24:23 +0800

    Question26

--- src/main/java/leetcode/test.java
+++ src/main/java/leetcode/test.java
@@ -11,23 +11,7 @@ import java.util.Objects;
  */
 public class test {
     public static void main(String[] args) {
-        Question25 test=new Question25();
-        Question25.ListNode h1=new Question25.ListNode(1);
-        Question25.ListNode h2=new Question25.ListNode(2);
-//        Question25.ListNode h3=new Question25.ListNode(3);
-//        Question25.ListNode h4=new Question25.ListNode(4);
-//        Question25.ListNode h5=new Question25.ListNode(5);
-
-        h1.next=h2;
-//        h2.next=h3;
-//        h3.next=h4;
-//        h4.next=h5;
-
-
-        Question25.ListNode result=test.reverseKGroup(h1,2);
-        while (result!=null){
-            System.out.print(result.val+" ");
-            result=result.next;
-        }
+        Question26 test=new Question26();
+        System.out.println(test.removeDuplicates(new int[]{1,1,2}));
     }
 }
